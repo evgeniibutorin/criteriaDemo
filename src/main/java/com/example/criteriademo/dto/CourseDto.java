f8b67cfada8d898311776f704c96baaf1de2bc74
Evgenii Butorin <evgeniibutorin@mail.ru>
2021-06-01 10:27:47 +0500

    before refactoring

--- src/main/java/com/example/criteriademo/dto/CourseDto.java
+++ src/main/java/com/example/criteriademo/dto/CourseDto.java
@@ -2,6 +2,8 @@ package com.example.criteriademo.dto;
 
 import com.example.criteriademo.model.Student;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -9,9 +11,20 @@ import java.util.Set;
 @Data
 public class CourseDto {
 
+    @Getter
+    @Setter
     int id;
+
+    @Getter
+    @Setter
     String courseName;
+
+    @Getter
+    @Setter
     int cost;
+
+    @Getter
+    @Setter
     Set<Student> students = new HashSet<>();
 }
 
--- src/main/java/com/example/criteriademo/dto/StudentDto.java
+++ src/main/java/com/example/criteriademo/dto/StudentDto.java
@@ -1,7 +1,10 @@
 package com.example.criteriademo.dto;
 
 import com.example.criteriademo.model.Course;
+import com.example.criteriademo.model.Student;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -9,7 +12,35 @@ import java.util.Set;
 @Data
 public class StudentDto {
 
+    @Getter
+    @Setter
     int id;
+
+    @Getter
+    @Setter
     String studentName;
-    Set<Course> courses = new HashSet<>();
+
+    @Getter
+    @Setter
+    Set<Course> courses;
+
+//    public Set<StudentDto> getStudentDtoSet(Set<Student> studentSet){
+//        Set<StudentDto> studentDtoSet = new HashSet<>();
+//        for (Student student:studentSet){
+//            courses = new HashSet<>();
+//            StudentDto studentDto = new StudentDto();
+//            studentDto.setId(student.getId());
+//            studentDto.setStudentName(student.getStudentName());
+//            for (Course course:student.getCourses()){
+//                CourseDto courseDto = new CourseDto();
+//                courseDto.setId(course.getId());
+//                courseDto.setCourseName(course.getCourseName());
+//                courseDto.setCost(course.getCourseCost());
+//                courses.add(courseDto);
+//            }
+//            studentDto.setCourses(courses);
+//            studentDtoSet.add(studentDto);
+//        }
+//        return studentDtoSet;
+//    }
 }
